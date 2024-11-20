3fc4dda9142ea9cf0ad32dd261b6d93ac79108c4
felipe-dev-gpa <gdogamergamer@gmail.com>
2024-11-19 21:49:23 -0300

    ULTIMA ATUALIZAÇÃO PRÉ TCC
    hospedagem firebase www. e requisição de 11 digitos implementada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-  apiKey: "AIzaSyDXNwwgNWSFvLM0QyBvBNuT9pG7CM6stSY",
-  authDomain: "estoque-fa87c.firebaseapp.com",
-  projectId: "estoque-fa87c",
-  storageBucket: "estoque-fa87c.appspot.com",
-  messagingSenderId: "984704213128",
-  appId: "1:984704213128:web:abff83d5a0ed41e2febf3d",
-  measurementId: "G-WNM5Y4MH8S"
+    apiKey: "AIzaSyDXNwwgNWSFvLM0QyBvBNuT9pG7CM6stSY",
+    authDomain: "estoque-fa87c.firebaseapp.com",
+    databaseURL: "https://estoque-fa87c-default-rtdb.firebaseio.com",
+    projectId: "estoque-fa87c",
+    storageBucket: "estoque-fa87c.firebasestorage.app",
+    messagingSenderId: "984704213128",
+    appId: "1:984704213128:web:abff83d5a0ed41e2febf3d",
+    measurementId: "G-WNM5Y4MH8S"
   }
 };
